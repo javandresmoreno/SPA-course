08e1d06a06a01ca58869b431951d7512b67fae4c
Javier Moreno <javier@solido.co>
2020-07-25 22:31:12 -0500

    testing my styles - 2

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
     <div class="Header-main">
       <div class="Header-logo">
         <h1>
-          <a href="/">
+          <a href="#/">
            <img src="${Logo}"/> 
           </a>
         </h1>
